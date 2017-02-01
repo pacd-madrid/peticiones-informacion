132c0dd60b37314eea9046c440321315405a2a63
Alfredo Sánchez Alberca <asalber@gmail.com>
2017-02-01 17:57:58 +0100

    Cambio de base de datos a Airtable y presentación de las peticiones en forma de tabla

--- peticiones-informacion.R
+++ peticiones-informacion.R
@@ -1,66 +1,67 @@
 # Script para publicar el registro de peticiones de información de los grupos auditores de los distritos al Ayuntamiento de Madrid 
-
-# URL de la hoja de cálculo con los datos
-url.data <- "https://docs.google.com/spreadsheets/d/1L_kLylhVReaL7GiL1Q0PJjx_b6yWOf2EPuk4bxiOIEU/pub?gid=1250269168&single=true&output=csv"
 library(RCurl)
 library(rmarkdown)
 library(yaml)
+library(airtabler)
+library(dplyr)
 
-# Carga de los datos
-data <- read.csv(text = getURL(url.data, .encoding = "UTF-8"), encoding = "UTF-8", header = T, stringsAsFactors = F)
+# Carga de los datos desde Google Drive
+# url.data <- "https://docs.google.com/spreadsheets/d/1L_kLylhVReaL7GiL1Q0PJjx_b6yWOf2EPuk4bxiOIEU/pub?gid=1250269168&single=true&output=csv"
+# data <- read.csv(text = getURL(url.data, .encoding = "UTF-8"), encoding = "UTF-8", header = T, stringsAsFactors = F)
+
+# Carga de datos desde AIRTABLE
+Sys.setenv(AIRTABLE_API_KEY="keyZOgDPeLEn3Me8q")
+datos <- airtable(base = "appe7uLXI2oMZ033Y", tables = c("Peticiones", "Respuestas", "Distritos", "Fichas ilegitimidad"))
+peticiones <- datos$Peticiones$select()
+respuestas <- datos$Respuestas$select()
+distritos <- datos$Distritos$select()
+fichas <- datos$`Fichas ilegitimidad`$select()
+# PREPOCESAMIENTO DE DATOS
+peticiones$id <- NULL
+respuestas$id <- NULL
+peticiones$createdTime <- NULL
+respuestas$createdTime <- NULL
+distritos$createdTime <- NULL
+peticiones$Distrito <- unlist(peticiones$Distrito)
+peticiones$Caso <- unlist(peticiones$Caso)
+names(fichas)[names(fichas) == 'Título'] <- "Ficha"
+# Fusionar tabla de peticiones con tabla de distritos
+datos <- left_join(peticiones, distritos[c("id","Nombre")], by=c("Distrito"="id"))
+datos$Distrito <- NULL
+names(datos)[names(datos) == 'Nombre'] <- 'Distrito'
+# Fusionar tabla de peticiones con tabla de fichas de ilegitimidad
+datos <- left_join(datos, fichas[c("id","Ficha")], by=c("Caso"="id"))
+datos$Caso <- NULL
+names(datos)[names(datos) == 'Ficha'] <- 'Caso'
+# Fusionar tabla de peticiones con tabla de respuestas
+datos <- left_join(datos, respuestas, by="Registro")
+# Calcular tiempo de respuesta
+datos[["Tiempo respuesta"]] <- as.numeric(as.Date(datos[["Fecha respuesta"]])-as.Date(datos[["Fecha envío"]]))
 
 # Reordenar las columnas y eliminar el correo y el colectivo 
-data <- data[c(1:4,11,5:8,12)]
+#data <- data[c(1:4,11,5:8,12,13)]
 
 # Obtener normbres de variables para las cabeceras de sección
-headers <- gsub(".", " ", names(data), fixed=T)
+#headers <- gsub(".", " ", names(data), fixed=T)
 
 # Escala de satisfación
-data$Valoración.de.la.respuesta <- ifelse(is.na(data$Valoración.de.la.respuesta), 0, data$Valoración.de.la.respuesta) 
-satisfaccion <- c("Pendiente de valorar", "Nada satifactoria", "Poco satifactoria", "Moderadamente satisfactoria", "Bastante satisfactoria", "Completamente satisfactoria")
-data$Valoración.de.la.respuesta <- satisfaccion[data$Valoración.de.la.respuesta+1]
+#data$Valoración.de.la.respuesta <- ifelse(is.na(data$Valoración.de.la.respuesta), 0, data$Valoración.de.la.respuesta) 
+#satisfaccion <- c("Pendiente de valorar", "Nada satifactoria", "Poco satifactoria", "Moderadamente satisfactoria", "Bastante satisfactoria", "Completamente satisfactoria")
+#data$Valoración.de.la.respuesta <- satisfaccion[data$Valoración.de.la.respuesta+1]
 
 # Completar la fecha de respuesta cuando aún no hay respuesta
-data$Fecha.respuesta <- ifelse(data$Fecha.respuesta=="", "Aún sin respuesta", data$Fecha.respuesta) 
+#data$Fecha.respuesta <- ifelse(data$Fecha.respuesta=="", "Aún sin respuesta", data$Fecha.respuesta) 
 
-
-# Número de columnas a procesar (las dos últimas no se procesan al tener datos confidenciales)
-n <- ncol(data)
-
-#' Title
-#' Función que crea una cadena en formato Rmardown con el contenido de una sección de la ficha.
-#'
-#' @param name Cadena con el encabezado de la sección.
-#' @param field Cadena con el contenido de la sección.
-#'
-#' @return Cadena con una sección de la ficha en formato Rmarkdown.
-#' @export
-#'
-#' @examples
-render.field <- function(name, field){
-  return(paste("## ", name, "\n", field, "\n\n", sep=""))
+# GENERACIÓN DE FICHAS
+for (i in 1:nrow(datos)){
+  peticion = datos[i,]
+  render('ficha_peticion.Rmd',
+         output_format = "md_document", 
+         output_file =  paste("peticion-", gsub("/","-",peticion$Registro) , ".Rmd", sep=''),
+         output_dir = '.',
+         quiet = TRUE)
 }
-
-#' Title
-#' Función que crea un fichero con el contenido de una ficha en formato Rmarkdown. 
-#' El nombre del fichero en formato Rmarkdown se toma del segundo campo que se supone es el título de ficha.
-#' @param x Vector con los encabezados de sección.
-#' @param y Vector con los contenidos de las secciones.
-#'
-#' @return None
-#' @export
-#'
-#' @examples
-render.record <- function(x, y){
-  file.name <- paste("peticion-", gsub(":", "-", gsub("/", "-", gsub(" ", "-", y[1]))), ".Rmd", sep="")
-  file.create(file.name)
-  yamlheader <- as.yaml(list(title=paste("Petición", as.character(y[1]))))
-  write(paste("---\n", yamlheader,"---\n\n", sep=""), file=file.name, append=T)
-  write(unlist(Map(render.field, x[2:n], y[2:n])), file=file.name, append=T)
-  render_site(file.name)
-}
-
-# Generar las peticiones
-lapply(1:nrow(data), function(i) render.record(headers, data[i,]))
 
 render_site()
+# clean_site()
+
